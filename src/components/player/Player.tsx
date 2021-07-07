4a50f6a58d79ea65262774e3d1fdcbda932277ba
madhead <siarhei.krukau@gmail.com>
2021-07-08 01:48:05 +0200

    Video player

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
-import { useContext } from 'react';
 import Overlay from '../overlay';
-import { ApiContext } from '../../hooks/ApiProvider';
-import { Store } from '../../types/store';
-import { Api } from '../../types/api';
+import flvjs from 'flv.js';
 
-const Player: React.FC<{}> = () => {
-    const data = useContext<(Api & Store) | undefined>(ApiContext);
+class Player extends React.Component {
+  private videoNode!: HTMLVideoElement
+  private player!: flvjs.Player
 
+  componentDidMount() {
+    this.player = flvjs.createPlayer(
+      {
+        type: 'flv',
+        isLive: true,
+        url: 'http://18.192.37.20:8080/live/livestream.flv',
+        hasAudio: true,
+        hasVideo: true,
+      },
+      {
+        enableStashBuffer: true,
+        stashInitialSize: 128,
+      }
+    )
+
+    flvjs.LoggingControl.enableError = false;
+    flvjs.LoggingControl.enableWarn = true;
+
+    this.player.attachMediaElement(this.videoNode);
+    this.player.load();
+    this.player.play(); // REQUIRES BROWSER PERMISSION!!!
+    this.player.on('error', (err) => {
+      console.log(err);
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  render() {
     return (
-        <>
-            <video>
-                <source src={`${process.env.REACT_APP_VIDEO_URL}/${data?.videoId}`} />
-            </video>
-            <Overlay zIndex={1} />
-        </>
+      <>
+        <video
+          controls={true}
+          ref={node => this.videoNode = node!}
+          height='100%'
+          width='100%'
+        />
+        <Overlay zIndex={1} />
+      </>
     );
-};
+  }
+}
 
 export default Player;
